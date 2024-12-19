f3baf89fe11af2a136269dfd359f5ff406a56423
OnwardForward <89054067+OnwardForward@users.noreply.github.com>
2024-12-19 12:52:59 -0500

    ...

--- src/Weather.js
+++ src/Weather.js
@@ -33,7 +33,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "eae061c95483dd066657bfc7525418ed";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
