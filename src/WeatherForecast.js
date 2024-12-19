4f2eb5986cdcb3daef4d488f75ad4ad1304fe4ba
OnwardForward <89054067+OnwardForward@users.noreply.github.com>
2024-12-19 12:23:50 -0500

    ..

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "722b06d0447t5a93do6af4432b6d8fb1";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
